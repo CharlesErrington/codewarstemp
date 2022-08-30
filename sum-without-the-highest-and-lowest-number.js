75eac90acf741eca23d85c148f013282fe543a58
Charles Errington <charleserringtondev@gmail.com>
2022-08-30 17:38:43 +0100

    add solution 01 Aug 30 2022

---
+++ sum-without-the-highest-and-lowest-number.js
@@ -0,0 +1,18 @@
+// Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+
+// The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+
+// Mind the input validation.
+
+// { 6, 2, 1, 8, 10 } => 16
+// { 1, 1, 11, 2, 3 } => 6
+
+//My solution
+
+function sumArray(array) {
+    if(array == null || !array.length ) return 0;
+    array.sort((a, b) => a-b);
+    let sum = 0;
+    for(let i = 1; i < array.length-1; i++)sum+=array[i];
+    return sum;
+  }
